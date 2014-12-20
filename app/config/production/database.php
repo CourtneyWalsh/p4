43c177827dea37796cfb5fc762812fba4a88b7c9
courtneywalsh <courtneywalsh@g.harvard.edu>
2014-12-20 16:28:35 -0500

    add production file

---
+++ app/config/production/database.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+
+    'connections' => array(
+
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => 'bucketlist',
+            'username'  => 'root',
+            'password'  => 'duYmf2E7VS',
+        ),
+
+    ),
+
+);
