21d790a9984f7ce5a8b8d965ba5707009c14c6b6
courtneywalsh <courtneywalsh@g.harvard.edu>
2014-12-20 17:53:22 -0500

    seeder page

--- app/database/seeds/BucketlistSeeder.php
+++ app/database/seeds/BucketlistSeeder.php
@@ -5,6 +5,13 @@ class BucketlistSeeder extends Seeder {
   public function run()
   {
   
+ 	$user = new User;
+		$user->email      = 'cnw353@gmail.com';
+		$user->password   = Hash::make('Courtney1');
+		$user->first_name = 'Courtney';
+		$user->last_name  = 'Walsh';
+		$user->save();
+
   }
  
 }
